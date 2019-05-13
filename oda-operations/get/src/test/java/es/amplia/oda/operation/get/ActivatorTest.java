9e602adb0df067c699dcf49ea4da0571cac4e3a9
Francisco Javier de Arcos <javier.dearcos@amplia.es>
2019-05-13 14:55:47 +0200

    Refactor Get operation using State Manager

---
+++ oda-operations/get/src/test/java/es/amplia/oda/operation/get/ActivatorTest.java
@@ -0,0 +1,62 @@
+package es.amplia.oda.operation.get;
+
+import es.amplia.oda.core.commons.osgi.proxies.StateManagerProxy;
+import es.amplia.oda.operation.api.OperationGetDeviceParameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Activator.class)
+public class ActivatorTest {
+
+    private final Activator testActivator = new Activator();
+
+    @Mock
+    private BundleContext mockedContext;
+    @Mock
+    private StateManagerProxy mockedStateManager;
+    @Mock
+    private OperationGetDeviceParametersImpl mockedGetDeviceParametersImpl;
+    @Mock
+    private ServiceRegistration<OperationGetDeviceParameters> mockedRegistration;
+
+    @Test
+    public void testStart() throws Exception {
+        PowerMockito.whenNew(StateManagerProxy.class).withAnyArguments().thenReturn(mockedStateManager);
+        PowerMockito.whenNew(OperationGetDeviceParametersImpl.class).withAnyArguments()
+                .thenReturn(mockedGetDeviceParametersImpl);
+        when(mockedContext.registerService(eq(OperationGetDeviceParameters.class), any(OperationGetDeviceParameters.class), any()))
+                .thenReturn(mockedRegistration);
+
+        testActivator.start(mockedContext);
+
+        PowerMockito.verifyNew(StateManagerProxy.class).withArguments(eq(mockedContext));
+        PowerMockito.verifyNew(OperationGetDeviceParametersImpl.class).withArguments(mockedStateManager);
+        when(mockedContext.registerService(eq(OperationGetDeviceParameters.class), eq(mockedGetDeviceParametersImpl), any()));
+    }
+
+    @Test
+    public void testStop() {
+        Whitebox.setInternalState(testActivator, "stateManager", mockedStateManager);
+        Whitebox.setInternalState(testActivator, "registration", mockedRegistration);
+
+        testActivator.stop(mockedContext);
+
+        verify(mockedRegistration).unregister();
+        verify(mockedStateManager).close();
+    }
+}
