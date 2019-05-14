778469637c08cb3e922922048f04da5f33181f43
Francisco Javier de Arcos <javier.dearcos@amplia.es>
2019-05-14 10:43:52 +0200

    Add set value methods to State Manager API

--- oda-core/commons/src/main/java/es/amplia/oda/core/commons/interfaces/StateManager.java
+++ oda-core/commons/src/main/java/es/amplia/oda/core/commons/interfaces/StateManager.java
@@ -1,5 +1,6 @@
 package es.amplia.oda.core.commons.interfaces;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -7,4 +8,6 @@ public interface StateManager {
     CompletableFuture<DatastreamValue> getDatastreamInformation(String deviceId, String datastreamId);
     CompletableFuture<Set<DatastreamValue>> getDatastreamsInformation(String deviceId, Set<String> datastreamIds);
     CompletableFuture<Set<DatastreamValue>> getDeviceInformation(String deviceId);
+    CompletableFuture<DatastreamValue> setDatastreamValue(String deviceId, String datastreamId, Object value);
+    CompletableFuture<Set<DatastreamValue>> setDatastreamValues(String deviceId, Map<String, Object> datastreamValues);
 }
--- oda-core/commons/src/main/java/es/amplia/oda/core/commons/osgi/proxies/StateManagerProxy.java
+++ oda-core/commons/src/main/java/es/amplia/oda/core/commons/osgi/proxies/StateManagerProxy.java
@@ -5,6 +5,7 @@ import es.amplia.oda.core.commons.interfaces.StateManager;
 
 import org.osgi.framework.BundleContext;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -29,6 +30,16 @@ public class StateManagerProxy implements StateManager, AutoCloseable {
     @Override
     public CompletableFuture<Set<DatastreamValue>> getDeviceInformation(String deviceId) {
         return proxy.callFirst(stateManager -> stateManager.getDeviceInformation(deviceId));
+    }
+
+    @Override
+    public CompletableFuture<DatastreamValue> setDatastreamValue(String deviceId, String datastreamId, Object value) {
+        return proxy.callFirst(stateManager -> stateManager.setDatastreamValue(deviceId, datastreamId, value));
+    }
+
+    @Override
+    public CompletableFuture<Set<DatastreamValue>> setDatastreamValues(String deviceId, Map<String, Object> datastreamValues) {
+        return proxy.callFirst(stateManager -> stateManager.setDatastreamValues(deviceId, datastreamValues));
     }
 
     @Override
