aa984b648abc61c09bdc131dd6bd37f8f8765625
Francisco Javier de Arcos <javier.dearcos@amplia.es>
2019-10-17 13:02:42 +0200

    Only install ODA external dependencies if they are not cached

--- install-oda-external-dependencies.sh
+++ install-oda-external-dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd oda-externaldependencies
-mvn install
+if [ ! -d ${HOME}/.m2/repository/com/automatak/dnp3 ] && [ ! -d ${HOME}/.m2/repository/com/diozero/libdiozero-system-utils ] && [ ! -d ${HOME}/.m2/repository/net/java/openjdk ]; then
+    cd oda-externaldependencies
+    mvn install
+fi
+
